dead711f70f1f9ea17355eeb13f6fbceadbd5307
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-12-03 20:11:27 +0100

    branded new design

--- src/aping-design-xgallerify.js
+++ src/aping-design-xgallerify.js
@@ -1,7 +1,7 @@
 "use strict";
 
-angular.module('jtt_aping_design_sample', [])
-    .controller('apingSampleDesignController', ['$scope', function ($scope) {
+angular.module('jtt_aping_design_xgallerify', [])
+    .controller('apingXgallerifyDesignController', ['$scope', function ($scope) {
 
         $scope.$on('apiNG.resultMerged', function () {
             $scope.workingCopy = $scope.results;
