d3b5c5d2785716dc97b96a903fba5ee8b5e3f5b8
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-01-23 23:55:22 +0100

    fixed new aping strukture

--- demo/js/aping-config.js
+++ demo/js/aping-config.js
@@ -2,17 +2,7 @@
 
 angular.module('jtt_aping').config(['$provide', function ($provide) {
 
-    $provide.constant("apingApiKeys", {
-
-    });
-
     $provide.constant("apingDefaultSettings", {
-        templateUrl : "../src/aping_design_xgallerify.html",
-        items : 20, //items per request
-        maxItems: 100, //max items per aping
-        orderBy : "timestamp",
-        orderReverse : "true",
-        model: "image",
         apingApiKeys: {
             twitter: [
                 {'bearer_token':'AAAAAAAAAAAAAAAAAAAAACs3iwAAAAAA%2BH8LdT7YfKIzk8fvQvqKvoePuxo%3DMKfXX2ojtacGL8aOLAhlBDUrEFpy0DUuiyebet22Wa2YVrX7cc'}
